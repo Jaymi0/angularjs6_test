0a2231f8810ae4ac33d1ef7185dbfb1cf402e859
Jemyh <jaming_h@163.com>
2018-10-27 18:06:05 +0800

    modify module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import {DatePipe} from '@angular/common';
 
 @NgModule({
+  // 可声明对象表 声明那些属于本NgModule的组件 指令或者管道
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
